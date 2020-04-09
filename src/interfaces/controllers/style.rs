0f83270d92d1d6d2c643e342eafd7163c2c2eed4
TaKO8Ki <takoyaki0316@gmail.com>
2020-04-09 23:49:52 +0900

    add debug

--- src/interfaces/controllers/style.rs
+++ src/interfaces/controllers/style.rs
@@ -6,6 +6,7 @@ use std::collections::HashMap;
 
 pub type PropertyMap = HashMap<String, Value>;
 
+#[derive(Debug)]
 pub struct StyledNode<'a> {
     pub node: &'a Node,
     pub specified_values: PropertyMap,
